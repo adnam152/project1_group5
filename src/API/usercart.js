380f7cc4e1763d1f5f75d595bdd2cb9506b81f7a
Tuấn Nam <teddy.nam152@gmail.com>
2023-09-02 15:50:17 +0700

    update cart 2/9 by adnam

--- src/API/usercart.js
+++ src/API/usercart.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import API_user from "./API_user";
 
 export default function userCart(){
-    const userId = storageUser.GET_localstorage().id;
+    const userId = storageUser.GET_localstorage()?.id;
     async function getUser(){
         let temp = await axios.get(API_user,{
             params : {
@@ -13,34 +13,43 @@ export default function userCart(){
         const [user] = temp.data;
         return user;
     }
+    async function updateUser(user){
+        await axios.put(API_user+`/${userId}`,{
+            ...user
+        });
+        storageUser.SET_localstorage({
+            id: user.id,
+            username: user.username,
+            cart: user.cart
+        });
+    }
     return {
         async GET(){
             return getUser();
         },
-        async SET(product){
+        async SET(product, option=''){
             let user = await getUser();
-            let isSame = false;
-            user.cart.forEach((item, index)=>{
-                if(item.id === product.productId && item.color === product.color && item.size === product.size){
-                    user.cart[index].quantity = parseInt(product.quantity) + parseInt(item.quantity);
-                    isSame = true;
-                }
+            let index = user.cart.findIndex(item =>{
+                return item.productId === product.productId && item.color === product.color && item.size === product.size
             })
-            if(!isSame){
-                user.cart.push(product);
-            };
-            await axios.put(API_user+`/${userId}`,{
-                ...user
-            });
-            storageUser.SET_localstorage({
-                id: user.id,
-                username: user.username,
-                cart: user.cart
-            });
+            if(index == -1) user.cart.push(product);
+            else if(option == 'update') user.cart[index].quantity = parseInt(product.quantity);
+            else user.cart[index].quantity = parseInt(product.quantity) + parseInt(user.cart[index].quantity);
+
+            updateUser(user);
             document.querySelector('header .quantity').innerText = user.cart.length
         },
-        async REMOVE(){
-
+        async REMOVE(id){
+            let user = await getUser();
+            let index = user.cart.findIndex(item=>{
+                return item.productId == id;
+            });
+            if(index != -1){
+                user.cart.splice(index,1);
+                updateUser(user);
+                document.querySelector('header .quantity').innerText = user.cart.length
+            }
+            else console.log('Can not found product');
         }
     }
 }
--- src/components/cartcomponent.js
+++ src/components/cartcomponent.js
@@ -14,31 +14,90 @@ export default function cartPageComponent() {
 
         let GET_cart = await userCart().GET();
         const carts = GET_cart.cart;
-        const tableContainer = document.querySelector('#cartPage table');
+        const tbody = document.querySelector('#cartPage table').querySelector('tbody');
 
-        function pricing(price, quantity) {
+        function getPrice(price) {
             const regex = /\$(\d+\.\d{2})/;
             const match = price.match(regex);
-            return match[1] * quantity
+            return parseFloat(match[1]);
         }
-        function renderTable(){
+        function totalPrice() {
+            const totalBox = document.querySelector('table .total-price');
+            const moneys = tbody.querySelectorAll('.money');
+            let currentValue = 0;
+
+            moneys.forEach(money => {
+                let tempMoney = money.getAttribute('data-money');
+                currentValue += parseFloat(tempMoney);
+            })
+            totalBox.innerHTML = currentValue
+        }
+        function renderTable() {
+            tbody.innerHTML = '';
             carts.forEach(async (cart) => {
                 let [product] = await getData({ id: cart.productId });
-                let html = `
-                        <tr>
+                let money = getPrice(product.price) * cart.quantity;
+                tbody.innerHTML += `
+                        <tr data-productId="${cart.productId}">
                             <td><a href="/products/${cart.productId}"><img src="${product.galeryImage[0]}">${product.name}</a></td>
-                            <td>${cart.color}</td>
-                            <td>${cart.size}</td>
-                            <td>${product.price}</td>
-                            <td>${cart.quantity}</td>
-                            <td>${'$' + pricing(product.price, cart.quantity)}</td>
+                            <td data-color="${cart.color}" class="color">${cart.color}</td>
+                            <td data-size="${cart.size}" class="size">${cart.size}</td>
+                            <td data-price="${product.price}" class="price">${product.price}</td>
+                            <td><span class="reduce">-</span><input type="number" readonly value="${cart.quantity}" class="quantity"><span class="increase">+</span></td>
+                            <td>$<span class="money" data-money="${money}">${money}</span></td>
+                            <td><button class="del">X</button></td>
                         </tr>
                     `
-                tableContainer.innerHTML += html;
+                totalPrice();
             })
         }
-        renderTable(); 
+        renderTable();
+        tbody.onclick = async function (e) {
+            const thisNode = e.target;
+            const thisRow = thisNode.closest('tr');
+            if (thisNode.className === 'del') {
+                const productId = thisNode.closest('tr').getAttribute('data-productId');
+                let temp = await userCart().REMOVE(productId);
+                thisRow.remove();
+                totalPrice();
+            }
+            function updateQuantity(quantity) {
+                const productId = thisRow.getAttribute('data-productId');
+                const color = thisRow.querySelector('.color').getAttribute('data-color');
+                const size = thisRow.querySelector('.size').getAttribute('data-size');
+                let product = {
+                    productId,
+                    color,
+                    size,
+                    quantity
+                }
+                userCart().SET(product, 'update');
+                
+                const price = getPrice(thisRow.querySelector('.price').getAttribute('data-price'));
+                const moneyElement = thisRow.querySelector('.money');
+                moneyElement.setAttribute('data-money',price*quantity);
+                moneyElement.innerHTML = price*quantity;
+                totalPrice();
+            }
+            if (thisNode.className == 'reduce') {
+                const quantity = thisRow.querySelector('.quantity');
+                let quantityValue = parseInt(quantity.value);
+                if (quantityValue > 1) {
+                    quantityValue--;
+                    quantity.value = quantityValue;
+                    updateQuantity(quantityValue);
+                };
+            }
+            if (thisNode.className == 'increase') {
+                const quantity = thisRow.querySelector('.quantity');
+                let quantityValue = parseInt(quantity.value);
+                quantityValue++;
+                quantity.value = quantityValue;
+                updateQuantity(quantityValue);
+            }
+        }
     })();
+
     return `
         <div class="cart_title">
             <div class="image"><img src="" alt=""> </div>
@@ -46,13 +105,20 @@ export default function cartPageComponent() {
         </div>
         <div class="table_products">
             <table border="1">
+                <thead>
+                    <tr>
+                        <th>Product</th>
+                        <th>Color</th>
+                        <th>Size</th>
+                        <th>Unit Price</th>
+                        <th>Quantity</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody></tbody>
                 <tr>
-                    <th>Product</th>
-                    <th>Color</th>
-                    <th>Size</th>
-                    <th>Unit Price</th>
-                    <th>Quantity</th>
-                    <th>Total Price</th>
+                    <td><b>Total Price</b></td>
+                    <td colspan=6>$<span class="total-price">0</span></td>
                 </tr>
             </table>
         </div>
